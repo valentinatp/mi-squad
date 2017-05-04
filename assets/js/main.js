9d003270620d147e0cfa0627251f992e573926bd
valentinatp <valentina.toledoporma@gmail.com>
2017-05-04 19:34:13 -0300

    añado la funcion para poder imprimir datos en html, luego en html creo un 'div' con un 'id' y creo un 'div' tambien con un 'id' para poder imprimir en el html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,10 @@ function Misquad(nombre, apellido, edad, hobbies){
 	this.apellido = apellido;
 	this.edad = edad;
 	this.hobbies = hobbies;
-
+	this.datos = function(){
+	var impr = document.getElementById("datitos");
+	var conte = document.getElementsByClassName("datote");
+	conte[0].innerHTML += "<b>Nombre: </b>" + this.nombre + " " + this.apellido + "<br>" + "<b>Edad: </b>" + this.edad + "<br>" + "<b>Hobbies: </b>" + "<li>"+this.hobbies + "<br>" + "<br>";
+	}
+}
 
