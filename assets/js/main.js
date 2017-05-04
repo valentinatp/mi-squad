53307170aa47b1bcdcd79b6bb8dfb36f0a144971
valentinatp <valentina.toledoporma@gmail.com>
2017-05-04 19:35:29 -0300

    Creo las variables con los nombres de las integrantes del squad

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,17 @@ function Misquad(nombre, apellido, edad, hobbies){
 	}
 }
 
+	var pez = new Misquad("Maria Paz", "Rojas", 26, "escuchar musica, comer, hablar ingles");
+	pez.datos()
+	var dani = new Misquad("Daniela", "Sanchez", 24, "escuchar musica, cantar, ver series");
+	dani.datos()
+	var diana = new Misquad("Diana", "Monsalve", 29, "hacer scratch, escuchar musica, negocios");
+	diana.datos()
+	var trini = new Misquad("Trinidad", "Fuentes", 26, "video juegos, puglover, dormir");
+	trini.datos()
+	var vale = new Misquad("Valentina", "Lopez", 25, "tarot, idiomas, peliculas mudas");
+	vale.datos()
+	var mari = new Misquad("Maribel", "Diaz", 30, "salir a tomar cafe, comer muffins, escribir");
+	mari.datos()
+	var gilia = new Misquad("Gilia", "Gongora", 28, "eschuchar musica, cantar, bailar");
+	gilia.datos()
