9eadb5a0db089c8c896fe9ce3a0349aa3388ac93
valentinatp <valentina.toledoporma@gmail.com>
2017-05-06 00:22:29 -0300

    Agrego un push a cada variable de las integrantes para agregarlas en el arreglo vacio

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,17 @@ function Misquad(nombre, edad, hobbies){
 var integrantes = [];
 
 	var pez = new Misquad("Maria Paz Rojas", 26, ["Escuchar musica", "Comer", "Hablar ingles"]);
+	integrantes.push(pez);
 	var dani = new Misquad("Daniela Sanchez", 24, ["Escuchar musica", "Cantar", "Ver series"]);
-
+	integrantes.push(dani);
 	var diana = new Misquad("Diana Monsalve", 29, ["Hacer scratch", "Escuchar musica", "Negocios"]);
-	
+	integrantes.push(diana);
 	var trini = new Misquad("Trinidad Fuentes", 26, ["Video juegos", "Puglover", "Dormir"]);
-	
+	integrantes.push(trini);
 	var vale = new Misquad("Valentina Lopez", 25, ["Tarot", "Idiomas", "Peliculas mudas"]);
-
+	integrantes.push(vale);
 	var mari = new Misquad("Maribel Diaz", 30, ["Salir a tomar cafe", "Comer muffins", "Escribir"]);
-
+	integrantes.push(mari);
 	var gilia = new Misquad("Gilia Gongora", 28, ["Eschuchar musica", "Cantar", "Bailar"]);
-	
+	integrantes.push(gilia);
 
